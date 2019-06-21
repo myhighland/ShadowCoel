cd310f66aaa9025f06da52d336c2b92840c7b14d
TheWanderingCoel <39617891+TheWanderingCoel@users.noreply.github.com>
2019-06-21 08:53:20 +0800

    更新订阅Model

--- ShadowCoelModel/Subscribe.swift
+++ ShadowCoelModel/Subscribe.swift
@@ -31,5 +31,31 @@ extension SubscribeError: CustomStringConvertible {
 
 open class Subscribe: BaseModel {
     @objc open dynamic var name: String = ""
+    @objc open dynamic var update: Bool = true
     @objc open dynamic var url: String = ""
+    
+    public static let subscribePrefix = "subscribe://"
+}
+
+// Public
+extension Subscribe {
+    open func Uri() -> String {
+        if let name = Optional(name),
+        let url = Optional(url) {
+            let name_base64 = name.data(using: .utf8)?.base64EncodedString()
+            let name_base64_1 = name_base64!.replacingOccurrences(of: "=", with: "")
+            let name_base64_2 = name_base64_1.replacingOccurrences(of: "/", with: "_")
+            let url_base64 = url.data(using: .ascii)?.base64EncodedString()
+            let url_base64_1 = url_base64!.replacingOccurrences(of: "=", with: "")
+            let url_base64_2 = url_base64_1.replacingOccurrences(of: "/", with: "_")
+            let subscribe_uri = "\(name_base64_2):\(url_base64_2)"
+            return "subscribe://" + subscribe_uri
+        }
+        return ""
+    }
+    
+    public class func uriIsSubscribe(_ uri: String) -> Bool {
+        return uri.lowercased().hasPrefix(Subscribe.subscribePrefix)
+    }
+    
 }
